f3a31dc65fada9aaeb5c861dfca2137ba7b3e7ad
ivanoviliyan <ivanov.iliyan_mail@yahoo.com>
2025-04-03 00:08:25 +0300

    sad

--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -2,14 +2,13 @@ import React, { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 
 export default function Profile() {
-    const { authData } = useContext(UserContext);
-    console.log(user);
+    const { email } = useContext(UserContext);
 
     return (
         <>
             <div className="flex justify-center items-center pt-10 flex-col gap-5">
                 <p>
-                    Hi <span className="font-semibold">{user.username}</span>!
+                    Hi <span className="font-semibold">{email}</span>!
                 </p>
                 <p>This is your profile page!</p>
             </div>
